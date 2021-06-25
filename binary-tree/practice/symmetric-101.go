1d6a338b382ae502ac4e433664272f5c739ca4c2
penn <penn@pennhuang-mc0.tencent.com>
2021-06-25 13:24:38 +0800

    对称二叉树

--- binary-tree/practice/symmetric-101.go
+++ binary-tree/practice/symmetric-101.go
@@ -1,3 +1,5 @@
+// 对称二叉树
+
 package main
 
 import "fmt"
@@ -13,7 +15,10 @@ func main() {
 
 	root := buildTree()
 	isSymmetric := isSymmetricTree(root)
-	fmt.Printf("isSymmetric:%v", isSymmetric)
+	fmt.Printf("recursion isSymmetric:%v\n", isSymmetric)
+
+	isSymmetric = isSymmetricTreeIteration(root)
+	fmt.Printf("iteration isSymmetric:%v\n", isSymmetric)
 }
 
 func buildTree() (root *TreeNode) {
@@ -54,7 +59,7 @@ func buildTree() (root *TreeNode) {
 	return
 }
 
-// 是否对称二叉树
+// 是否对称二叉树 (递归)
 func isSymmetricTree(root *TreeNode) (isSymmetric bool) {
 	// 递归终止条件:
 	// 1. 两个节点都为空，则当前子树为镜像对称
@@ -64,6 +69,7 @@ func isSymmetricTree(root *TreeNode) (isSymmetric bool) {
 	var symmetircTree func(left *TreeNode, right *TreeNode) bool
 	symmetircTree = func(left *TreeNode, right *TreeNode) bool {
 		if left == nil && right == nil {
+			// 递归是求解子节点的答案，故子节点对称
 			return true
 		}
 
@@ -82,8 +88,54 @@ func isSymmetricTree(root *TreeNode) (isSymmetric bool) {
 	return
 }
 
-// // 是否镜像对称二叉树（迭代方法）
-// func isSymmetricTreeIteration(root *TreeNode) (isSymmetric bool) {
-// 	//
-// 	return
-// }
+// 是否镜像对称二叉树（迭代方法）
+func isSymmetricTreeIteration(root *TreeNode) (isSymmetric bool) {
+	// 当连个子树根节点相等时，就比较
+	// 左子树的left和右子树的right，这里可以用队列来实现
+	// 1. 从队列中拿出两个节点(left 和 right)比较
+	// 2. 将left.Left和right.Right节点放入队列
+	// 3. 讲left.Right和right.Left节点放入队列
+
+	if root == nil {
+		return true
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return true
+	}
+
+	// 生成队列保存节点
+	queue := []*TreeNode{}
+	// 将根节点左右孩子加入队列
+	queue = append(queue, root.Left, root.Right)
+	// queue = append(queue, root, root)
+
+	// 遍历队列
+	for len(queue) > 0 {
+		// 从队头取出两个节点
+		l, r := queue[0], queue[1]
+		queue = queue[2:]
+
+		// 两节点是否同时为nil，若真，则表示该两点均为叶子节点的子节点，跳过，继续循环遍历其他节点即可
+		if l == nil && r == nil {
+			// 迭代法使用的是BFS，此时不一定遍历完所有节点，故而继续循环
+			continue
+		}
+
+		// 某一节点为空，不对称
+		if l == nil || r == nil {
+			return false
+		}
+
+		// 对比节点值不相等，不对称
+		if l.Val != r.Val {
+			return false
+		}
+
+		// 此时两对比节点对称，则继续比较各自子节点
+		queue = append(queue, l.Left, r.Right)
+		queue = append(queue, l.Right, r.Left)
+	}
+
+	return true
+}
