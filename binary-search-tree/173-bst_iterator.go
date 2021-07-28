62745809f4ad16c0ab74774144b05025e32fbb7f
penn <penn_z@aliyun.com>
2021-07-29 01:22:47 +0800

    [173]二叉树搜索迭代树, 中序遍历, 迭代, 迭代过程中维护栈到达next效果

--- binary-search-tree/173-bst_iterator.go
+++ binary-search-tree/173-bst_iterator.go
@@ -49,3 +49,45 @@ func (this *BSTIterator) Next() int {
 func (this *BSTIterator) HasNext() bool {
 	return len(this.arrs) > 0
 }
+
+/*
+	思路2:
+		迭代版
+		1. 中序遍历--迭代
+		2. 此时无需预先计算出中序遍历的全部结果，只需要实时维护当前的栈情况即可
+*/
+type BSTIteratorV2 struct {
+	stack   []*TreeNode
+	curNode *TreeNode
+}
+
+func ConstructorV2(root *TreeNode) BSTIteratorV2 {
+	return BSTIteratorV2{
+		curNode: root,
+	}
+}
+
+func (this *BSTIteratorV2) NextV2() int {
+	// 这里注意，不再主动遍历stack，调用NextV2即可遍历
+
+	// base case
+
+	// this.curNode =
+	for this.curNode != nil {
+		// 入栈
+		this.stack = append(this.stack, this.curNode)
+		// 左
+		this.curNode = this.curNode.Left
+	}
+
+	// 弹栈
+	top := this.stack[len(this.stack)-1]
+	val := top.Val // 中
+	this.stack = this.stack[:len(this.stack)-1]
+	this.curNode = top.Right // 右
+	return val
+}
+
+func (this *BSTIteratorV2) HasNextV2() bool {
+	return this.curNode != nil || len(this.stack) > 0
+}
