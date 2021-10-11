0e8ca4c4806bc12a7832f0ef1c3e68a358baedbe
penn <penn_z@aliyun.com>
2021-10-11 13:57:28 +0800

    206-反转链表-递归法

--- linked-list/singly-linked-list/206-反转链表.go
+++ linked-list/singly-linked-list/206-反转链表.go
@@ -4,6 +4,16 @@
 
 	输入：head = [1,2,3,4,5]
 	输出：[5,4,3,2,1]
+
+	迭代法:
+		1. 定义cur记录当前节点，pre记录前一节点
+		2. 迭代过程中，变量temp保存cur的下一个节点，待更新cur时使用
+		3. 迭代过程中，cur.Next更新为pre; 最后移动pre与cur指针，继续下一次迭代
+
+
+	递归法:
+		1. 假设当前节点k后的节点都已反转，则k与k+1的反转关系为, k.next.next = k
+		2.
 */
 
 package main
@@ -32,4 +42,23 @@ func reverseList(head *ListNode) *ListNode {
 	}
 
 	return pre
+}
+
+// 递归法
+func reverseListRecusion(head *ListNode) *ListNode {
+	// base case
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	// 递归处理当前节点的下一个节点，得到处理好的后面节点的反转链表
+	ret := reverseList(head.Next)
+
+	// 下一个节点的next指向当前head
+	head.Next.Next = head
+
+	// 当前head.next置为nil，防止产生环
+	head.Next = nil
+
+	return ret
 }
