d88776c8a50b4e2662e93ae142a06a0e30095ae5
penn <penn_z@aliyun.com>
2021-11-30 18:03:54 +0800

    77-组合-回溯法-剪枝优化

--- 回溯算法/77-组合.go
+++ 回溯算法/77-组合.go
@@ -35,8 +35,21 @@ func backtrack(n, k int, path []int, startIndex int) {
 		return
 	}
 
+	/*
+		优化重点:
+			因为每层我们都是从startIndex起始位置开始遍历的，但是在 (startIndex+1)之后的数都不足以 满足我们需要的元素个数了，
+			可以不必再搜索，进行剪枝操作
+
+		优化过程:
+			1. 已选择的元素个数: len(path)
+			2. 还需要选择的元素个数: k - len(path)
+			3. 在集合n中至多要从该起始位置: n - (k - len(path)) + 1 开始搜索，才满足题意条件
+	*/
+
 	// for 选择 in 选择列表:
-	for i := startIndex; i <= n; i++ {
+	// i为本次搜索的起始位置
+	// for i := startIndex; i <= n; i++ { // 剪枝前
+	for i := startIndex; i <= n-(k-len(path))+1; i++ { // 剪枝后
 		// 排除不合法路径，该题无需该动作
 
 		// 选择，加入路径
