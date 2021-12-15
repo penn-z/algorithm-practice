53be7beb360dc95d7fe7d18a6e585cdb6f96e492
penn <penn_z@aliyun.com>
2021-12-15 13:55:34 +0800

    剑指offer-48-最大不含重复字符的子字符串

---
+++ 剑指offer/offer-48-最大不含重复字符的子字符串.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "abcabcbb" // 3
+	// str := "abba" // 2
+	res := lengthOfLongestSubstring(str)
+	fmt.Println("===res: ", res)
+
+}
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	// dp数组及下标含义
+	// dp[i]表示以当前str[i]结尾的无重复字符子串的长度
+
+	// 递推公式
+	/*
+		上一个与当前str[i]重复字符的位置即为j
+		1. 如果str[i]在dp[i-1]子串中出现，则从出现位置j到下一个位置i，构成了以str[i]结尾的不重复子串:
+			dp[i] = i - j
+		2. 如果str[i]不在dp[i-1]子串中出现，那么dp[i] = dp[i-1] + 1
+	*/
+
+	// dp初始化
+	dp := make([]int, len(s))
+	dp[0] = 1
+
+	// 哈希记录各字符最后出现的位置索引
+	lastShowMap := make(map[byte]int)
+	lastShowMap[s[0]] = 0
+
+	max := 1
+	for i := 1; i < len(s); i++ {
+		// 获取当前字符在前缀字符串中最后出现的位置
+		lastIndex, ok := lastShowMap[s[i]]
+		if ok {
+			if dp[i-1] < i-lastIndex {
+				// 当前字符不在dp[i-1]区间内出现
+				dp[i] = dp[i-1] + 1
+			} else if dp[i-1] >= i-lastIndex {
+				// 当前字符在dp[i-1]区间内出现
+				dp[i] = i - lastIndex
+			}
+		} else {
+			// 不在map里，故肯定不在dp[i-1]区间出现
+			dp[i] = dp[i-1] + 1
+		}
+
+		lastShowMap[s[i]] = i
+		if max < dp[i] {
+			max = dp[i]
+		}
+	}
+
+	return max
+}
