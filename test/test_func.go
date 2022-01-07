6d89ab395d3bb225f1dc452e365401b17bc4981f
penn <penn_z@aliyun.com>
2022-01-07 21:47:40 +0800

    test func

--- test/test_func.go
+++ test/test_func.go
@@ -3,9 +3,29 @@ package main
 import "fmt"
 
 func main() {
+	// testRangeOfIndex()
+	// maxInt()
+	testStrNum()
+}
+
+func testRangeOfIndex() {
 	// test 函数
 	vals := []int{1}
 
 	vals = vals[1:] // 不会越界
-	fmt.Printf("test:%v", vals)
+	fmt.Printf("test:%v\n", vals)
+}
+
+func maxInt() {
+	// 最大值
+	maxInt := int(^uint(0) >> 1)
+	// 最小值
+	minInt := int(^maxInt)
+	fmt.Println(maxInt)
+	fmt.Println(minInt)
+}
+
+func testStrNum() {
+	num1 := "111"
+	fmt.Println(num1[0] - '0')
 }
