847100e67393d074b4c489f9a4077ac284530182
penn <penn_z@aliyun.com>
2022-01-14 15:48:01 +0800

    堆排序巩固

--- 排序算法/堆排序2.go
+++ 排序算法/堆排序2.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	arr := []int{8, 2, 4, 7, 1, 5, 3}
@@ -8,63 +10,66 @@ func main() {
 	fmt.Println(arr)
 }
 
+// 建堆
 // 大顶堆，自上而下堆化
-// 此处的arr是从索引0开始的，索引0不是占位符
-// 则当前元素为i，左孩子为2i, 右孩子为2i+1
+// 当前元素为i，左孩子为2i, 右孩子为2i+1
 func buildHeap(arr []int, arrLen int) {
-	// 从最后一个非叶子节点开始，一次堆化
+	// 从最后一个非叶子节点开始，依次堆化
 	for i := arrLen/2 - 1; i >= 0; i-- {
 		heapify(arr, arrLen, i)
 	}
 
-	fmt.Println("建堆完成，arr: ", arr)
 }
 
 // 堆化
 func heapify(arr []int, arrLen int, i int) {
 	for {
-		// 假设当前元素与左右孩子节点中，当前元素最大，则maxPos是当前元素的索引
+		// 假设当前元素与左右孩子节点中，当前元素最大，maxPos为当前最大元素索引
 		maxPos := i
 		leftChild := 2 * i
 		rightChild := 2*i + 1
-		// 如果当前元素比其左孩子小，则maxPost需要替换为左孩子
-		if leftChild < arrLen && arr[i] < arr[leftChild] {
+
+		// maxPos与左孩子比较大小
+		if leftChild < arrLen && arr[maxPos] < arr[leftChild] {
 			maxPos = leftChild
 		}
 
-		// rightChild > max(i, leftchild)，则maxPos取rightChild
+		// maxPos与右孩子比较大小
 		if rightChild < arrLen && arr[maxPos] < arr[rightChild] {
 			maxPos = rightChild
 		}
 
-		// maxPos没有发生变化，说明当前i已经是最大元素，i堆化完成
+		// maxPos位置无变化，说明i堆化完成
 		if maxPos == i {
 			break
 		}
 
-		// 交换位置，即堆化
+		// 置换元素值
 		swap(arr, i, maxPos)
 
-		// 更新i位置，i往下移
+		// 更新i位置, i往下移
 		i = maxPos
 	}
 }
 
+// 排序
 func sort(arr []int) []int {
 	arrLen := len(arr)
-	// 先堆化
-	buildHeap(arr, len(arr))
+	// 建堆
+	buildHeap(arr, arrLen)
 	// 排序，不断把堆顶元素弹出，并保持堆化的过程
 	for i := arrLen - 1; i >= 0; i-- {
 		/*
-			堆顶第一个元素与最后一个元素交换位置
-				如果是大顶堆，则最后的数组从右往左降序，则正序为升序数组
+			堆顶元素与最后一个元素交换位置
+				如果是大顶堆，则最后的数组从右往左降序，则正序为升序
 				小顶堆反之，正序为降序数组
 		*/
 		swap(arr, 0, i)
+
 		// 数组长度减小
 		arrLen--
-		// 保持堆化
+
+		// 保持数组堆化 (堆顶元素是最后元素置换上来的，故需要对堆顶元素堆化操作)
 		heapify(arr, arrLen, 0)
 	}
 
